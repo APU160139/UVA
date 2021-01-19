482a2848ab8e77a0d87abd6bdb39062edf5af9de
MD APU HOSEN <47341461+APU160139@users.noreply.github.com>
2021-01-20 00:03:12 +0600

    Add files via upload

---
+++ 11877 - The Coco-Cola Store .cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m,n,c=1;
+    while(c<=10){
+     scanf("\n%d", &n);
+     if(n==0){
+        break;
+     }
+    ++c;
+    m=n/2;
+    printf("%d\n",m);
+   }
+    return 0;
+}
